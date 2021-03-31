0bec70f1b4d571912e55661f00f6ae51b32a4b02
piwiit <bouillaux@hotmail.fr>
2021-03-31 17:03:51 +0200

    add font family

--- script.js
+++ script.js
@@ -13,17 +13,16 @@ class Book {
 const showBooks = () => {
   document.querySelector('.cardContent').innerHTML = myLibrary.reduce(
     // eslint-disable-next-line no-return-assign
-    (content, book) => (content += `<div class="card" style="width: 18rem;">
-      <div class="card-body">
-        <h5 class="card-title">${book.title}</h5>
+    (content, book) =>
+      (content += `<div class="card" style="width: 18rem;">
+        <h3 class="title">${book.title}</h3>
         <ul >
           <li >${book.pages}</li>
           <li >${book.status}</li>
           <li >${book.author}</li>
         </ul>
-      </div>
     </div>`),
-    '',
+    ''
   );
 };
 
@@ -40,12 +39,7 @@ exempleButton.addEventListener('click', () => {
   addBookToLibrary('Blood of Elves', 'Andrzej Sapkowski', '320', true);
   addBookToLibrary('Time of Contempt', 'Andrzej Sapkowski', '331', true);
   addBookToLibrary('Baptism of Fire', 'Andrzej Sapkowski', '343', true);
-  addBookToLibrary(
-    'The Tower of the Swallow',
-    'Andrzej Sapkowski',
-    '436',
-    false,
-  );
+  addBookToLibrary('The Tower of the Swallow', 'Andrzej Sapkowski', '436', false);
   addBookToLibrary('The Lady of the Lake', 'Andrzej Sapkowski', '531', false);
   addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', '366', true);
   addBookToLibrary('The Alchemist', 'Paulo Coelho', '182', false);
