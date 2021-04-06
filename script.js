f3f586da21a04b3cd88ee70fd22e11ca8bb80e0d
piwiit <bouillaux@hotmail.fr>
2021-04-06 16:45:53 +0200

    add modal and style modal

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 /* eslint-disable no-param-reassign */
 const myLibrary = [];
 
+const cardsContent = document.querySelector('.cardContent');
+const span = document.querySelector('.close');
+const modal = document.querySelector('#myModal');
+const addBook = document.querySelector('#add-book');
+const checkbox = document.querySelector('#myCheck');
+const bookTitle = document.querySelector('#title');
+const bookAuthor = document.querySelector('#author');
+const bookPages = document.querySelector('#pages');
+const pushNewBook = document.querySelector('#push-new-book');
+
 class Book {
   constructor(title, author, pages, status) {
     this.title = title;
@@ -11,19 +21,18 @@ class Book {
 }
 
 const showBooks = () => {
-  document.querySelector('.cardContent').innerHTML = myLibrary.reduce(
+  cardsContent.innerHTML = myLibrary.reduce(
     // eslint-disable-next-line no-return-assign
-    (content, book) =>
-      (content += `<div class="card" >
+    (content, book) => (content += `<div class="card" >
         <h3 class="title">${book.title}</h3>
         <div class="separator"></div>
         <ul >
           <li >${book.pages} pages</li>
-          <li >${book.status}</li>
           <li >${book.author}</li>
+          <li >${book.status === true ? '<i class="fas fa-check"></i> Already Read' : '<i class="far fa-times-circle"></i> Not read yet'}</li>
         </ul>
     </div>`),
-    ''
+    '',
   );
 };
 
@@ -32,18 +41,46 @@ const addBookToLibrary = (title, author, pages, status) => {
   myLibrary.push(newBook);
   showBooks();
 };
-const exempleButton = document.querySelector('.exemple');
+const exempleButton = document.querySelector('#exemple');
 
-exempleButton.addEventListener('click', () => {
+exempleButton.onclick = () => {
   addBookToLibrary('The Last Wish', 'Andrzej Sapkowski', '288', true);
   addBookToLibrary('Sword of Destiny', 'Andrzej Sapkowski', '384', true);
   addBookToLibrary('Blood of Elves', 'Andrzej Sapkowski', '320', true);
   addBookToLibrary('Time of Contempt', 'Andrzej Sapkowski', '331', true);
   addBookToLibrary('Baptism of Fire', 'Andrzej Sapkowski', '343', true);
-  addBookToLibrary('The Tower of the Swallow', 'Andrzej Sapkowski', '436', false);
+  addBookToLibrary(
+    'The Tower of the Swallow',
+    'Andrzej Sapkowski',
+    '436',
+    false,
+  );
   addBookToLibrary('The Lady of the Lake', 'Andrzej Sapkowski', '531', false);
   addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', '366', true);
   addBookToLibrary('The Alchemist', 'Paulo Coelho', '182', false);
   addBookToLibrary('On the Road', 'Jack Kerouac', '307', false);
   addBookToLibrary('1984', 'George Orwell', '328', true);
-});
+};
+
+addBook.onclick = () => {
+  modal.style.display = 'block';
+  cardsContent.style.filter = 'blur(2px)';
+};
+
+span.onclick = () => {
+  modal.style.display = 'none';
+  cardsContent.style.filter = 'blur(0px)';
+};
+
+window.onclick = (e) => {
+  if (e.target === modal) modal.style.display = 'none';
+};
+
+pushNewBook.onclick = () => {
+  const title = bookTitle.textContent;
+  const author = bookAuthor.textContent;
+  const pages = bookPages.textContent;
+  const status = checkbox.checked;
+  console.log(title, author, pages, status);
+  addBookToLibrary(title, author, pages, status);
+};
